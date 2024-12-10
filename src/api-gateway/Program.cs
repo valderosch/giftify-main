8bd1b2afc8c2ab0866ecefde925408592bc026dc
valderosch <phonelix96@gmail.com>
2024-12-10 13:48:55 +0200

    gateway-cors

--- src/api-gateway/Program.cs
+++ src/api-gateway/Program.cs
@@ -6,11 +6,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
 builder.Services.AddOcelot();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("GatewayPolicy", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000") 
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
 // app.UseAuthentication();
+app.UseCors("GatewayPolicy");
 
 app.UseOcelot().Wait();
+
 
 app.Run();
